2002012b1532a02ec7b5ab3cb54fb802628bb3c9
bentom08 <bentom06@hotmail.co.uk>
2018-11-01 09:49:04 +0000

    Finished repo layer

---
+++ src/main/java/com/qa/account/accountapp/AccountRepositary.java
@@ -0,0 +1,56 @@
+package com.qa.account.accountapp;
+
+import static javax.transaction.Transactional.TxType.REQUIRED;
+import static javax.transaction.Transactional.TxType.SUPPORTS;
+
+import java.util.List;
+
+import javax.enterprise.inject.Default;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
+
+@Transactional(SUPPORTS)
+@Default
+public class AccountRepositary {
+
+
+	@PersistenceContext(unitName = "primary")
+	EntityManager em;
+	
+	public List<Account> getAllAccounts() {
+		TypedQuery<Account> query = em.createQuery("SELECT a FROM Account a", Account.class);
+		return query.getResultList();
+	}
+	
+	@Transactional(REQUIRED)
+	public String createAccount(Account account) {
+		em.persist(account);
+		return "{\"Message\": \"Account Created\"}";
+	}
+	
+	@Transactional(REQUIRED)
+	public String deleteAccount(Long id) {
+		Account account = em.find(Account.class, id);
+		if (account != null) {
+			em.remove(account);
+		}
+		return "{\"Message\": \"Account Successfully Deleted\"}";
+	}
+	
+	public Account findAnAccount(Long id) {
+		return em.find(Account.class, id);
+	}
+	
+	@Transactional(REQUIRED)
+	public String updateAccount(Account account, Long id) {
+		Account oldAccount = em.find(Account.class, id);
+		
+		oldAccount.setFirstName(account.getFirstName());
+		oldAccount.setLastName(account.getLastName());
+		oldAccount.setAccountNumber(account.getAccountNumber());
+		
+		return "{\"message\": \"movie sucessfully updated\"}";
+	}
+}
--- src/main/java/com/qa/account/accountapp/JSONUtil.java
+++ src/main/java/com/qa/account/accountapp/JSONUtil.java
@@ -3,18 +3,27 @@ package com.qa.account.accountapp;
 import java.io.IOException;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class JSONUtil {
 	
 	private ObjectMapper mapper = new ObjectMapper();
 	
-	public <T> T getObjectfromJSON(String json, Class <T> clazz) throws JsonMappingException, IOException {
-		return mapper.readValue(json, clazz);
+	public <T> T getObjectfromJSON(String json, Class <T> clazz) {
+		try {
+			return mapper.readValue(json, clazz);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 	
-	public String getJSONfromObject(Object object) throws JsonProcessingException {
-		return mapper.writeValueAsString(object);
+	public String getJSONfromObject(Object object) {
+		try {
+			return mapper.writeValueAsString(object);
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 }
